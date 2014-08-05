c880fefd20172a1559421dfecdc4852e25d93e75
Callum Locke <callum.locke@gmail.com>
2014-08-05 19:21:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# parse-ft-spreadsheet-key
+
+Simple function to get the key from a Google Spreadsheets URL or Bertha URL.
+
+
+## install
+
+### front-end
+
+```sh
+$ bower install --save parse-ft-spreadsheet-key
+```
+
+```html
+<script src="bower_components/parse-ft-spreadsheet-key/index.js"></script>
+```
+
+The function will be available as `IG.parseSpreadsheetKey(str)`.
+
+
+### node.js
+
+```sh
+$ npm install --save-dev  parse-ft-spreadsheet-key
+```
+
+```js
+var parseSpreadsheetKey = require('parse-spreadsheet-key');
+```
+
+
+## usage
+
+```js
+var key = parseSpreadsheetKey('https://docs.google.com/a/ft.com/spreadsheet/ccc?key=0Ajt08GcPGJRbdFJUZWZfZ6M1V0xDaTFBckJnNENC8Gc');
+
+// key === '0Ajt08GcPGJRbdFJUZWZfZ6M1V0xDaTFBckJnNENC8Gc'
+```
+
+Notes:
+
+* takes valid Bertha URL or Google Spreadsheets URL
+* if you pass a plain key on its own, it will return it untouched
+* throws an error if something invalid is found, unless you pass `true` as a second argument (`silent`), in which case it just returns null
---
+++ test.js
@@ -0,0 +1,70 @@
+/*jshint node:true*/
+/*global describe, it*/
+
+'use strict';
+
+var expect = require('chai').expect;
+var parseSpreadsheetKey = require('./index.js');
+
+describe('parse-ft-spreadsheet-key', function () {
+  var key = '0Ajt08GcPGJRbdFJUZWZfZ3M1V0xDaTFBckJnNENCSGc';
+  var gsURLs = [
+    'https://docs.google.com/a/ft.com/spreadsheet/ccc?key=' + key,
+    'https://docs.google.com/a/ft.com/spreadsheet/ccc?key=' + key + '#hash',
+    'https://docs.google.com/a/ft.com/spreadsheet/ccc?key=' + key + '&foo=bar',
+    'https://docs.google.com/a/ft.com/spreadsheet/ccc?key=' + key + '&foo=bar#hash',
+  ];
+  var berthaURLs = [];
+  ([
+    'bertha.ig.ft.com',
+    'spottiswood.herokuapp.com',
+    'staging.bertha.ig.ft.com',
+    'spottiswood-tupp.herokuapp.com'
+  ]).forEach(function (host) {
+    berthaURLs = berthaURLs.concat([
+      'http://' + host + '/view/publish/ig/' + key,
+      'http://' + host + '/republish/publish/ig/' + key + '/basic,foo',
+      'http://' + host + '/view/publish/ig/' + key + '?callback=hi'
+    ]);
+  });
+  var invalidStrings = [
+    'ab==gsgsc....asdfasdf 891 sd', // invalid chars
+    'https://mops.google.com/a/ft.com/spreadsheet/ccc?key=' + key, // wrong hostname
+    '0Ajt08GcPGJRbdFJUZWZfZ3M1V0xDaTFBckJnNENCSGc/', // slash at end
+    '0Ajt08GcPGJRbdFJUZWZfZ', // too short
+    '0Ajt08GcPGJRbdFJUZWZfZ3M1V0xDaTFBckJnNENCSGc0Ajt08GcPGJRbdFJUZWZfZ3M1V0xDaTFBckJnNENCSGc' // too long
+  ];
+
+
+  it('handles a plain key', function () {
+    expect(parseSpreadsheetKey(key)).to.equal(key);
+  });
+
+  it('handles a Google Spreadsheets URL', function () {
+    gsURLs.forEach(function (url) {
+      expect(parseSpreadsheetKey(url)).to.equal(key);
+    });
+  });
+
+  it('handles a Bertha URL at any of the known Bertha hosts', function () {
+    berthaURLs.forEach(function (url) {
+      expect(parseSpreadsheetKey(url)).to.equal(key);
+    });
+  });
+
+  it('throws on invalid strings', function () {
+    invalidStrings.forEach(function (str) {
+      expect(function () {
+        parseSpreadsheetKey(str);
+      }).to.throw(/Cannot parse spreadsheet key from value/);
+    });
+  });
+
+  it('returns null for invalid strings if "silent" flag is passed', function () {
+    invalidStrings.forEach(function (str) {
+      expect(function () {
+        expect(parseSpreadsheetKey(str, true)).to.equal(null);
+      }).to.not.throw(Error);
+    });
+  });
+});
